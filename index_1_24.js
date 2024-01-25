1439297ef323d098d3a7a3d9a980d6195b17c25f
USERNAME <moglesby.data@gmail.com>
2024-01-24 20:43:20 -0500

    submit code for question #1

--- index_1_24.js
+++ index_1_24.js
@@ -211,7 +211,13 @@ const dinos = [
   ];
   
   // 1. Given a dinosaur object and a key return the value of the corresponding key in the object. If no key is provided return the dinosaurId
-  
+  function findDinoKey(obj, key) {
+    if (key) {
+        return (obj[obj.key]);
+    } else {
+        return dinosaurId;
+    }
+  }
   // ------------------------------------------------------
   // 2. Given a dinosaur object and an mya (Millions of years ago) value, return a boolean indicating whether it was alive during that time or not
   
